3bd4f39428fea982f2e7a175ba4b1deb5200afa1
Farhaan Khan <farhaankhan.fk137@gmail.com>
2023-03-12 22:19:34 +1200

    EMP form ui completed

--- resources/views/staff/ems/create.blade.php
+++ resources/views/staff/ems/create.blade.php
@@ -5,198 +5,225 @@
 <div class="pd-ltr-20 xs-pd-20-10">
 	<div class="min-height-200px">
         <!-- horizontal Basic Forms Start -->
-        <div class="pd-20 card-box mb-30">
-                    <div class="clearfix">
-                        <div class="pull-left">
-                            <h4 class="text-blue h4">Employee Registration Form</h4>
-                            <p class="mb-30">All Fields are complusory</p>
-                        </div>
+        <div class="pd-30 card-box mb-30">
+            <div class="clearfix">
+                <div class="pull-left">
+                    <h4 class="text-blue h4">Employee Registration Form</h4>
+                    <p class="mb-30">All Fields are complusory</p>
+                </div>
+            </div>
+            <form>
+            <p><b>Personal Details</b></p>
+            <div class="row"> 
+                <div class="col-md-6 col-sm-12">
+                    <div class="form-group">
+                        <label>First Name</label>
+                        <input class="form-control" type="text" placeholder="First Name"/>
                     </div>
-                    <form>
-                    <div class="col-md-3 col-sm-12">
-                        <div class="form-group">
-                            <label>Text</label>
-                            <input
-                                class="form-control"
-                                type="text"
-                                placeholder="Johnny Brown"
-                            />
-                        </div>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <div class="form-group">
+                        <label>Last Name</label>
+                        <input class="form-control" type="text" placeholder="Last Name"/>
+                    </div>
+                </div>            
+            </div>
+
+            <div class="row"> 
+                <div class="col-md-6 col-sm-12">
+                    <div class="form-group">
+                        <label>TIN Number</label>
+                        <input class="form-control" type="text" placeholder="TIN Number"/>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <div class="form-group">
+                        <label>Photo</label>
+                        <input type="file" class="form-control-file form-control height-auto">
+                    </div>
+                </div>            
+            </div>
+
+            <div class="row"> 
+                <div class="col-md-6 col-sm-12">
+                    <div class="form-group">
+                        <label>Address</label>
+                        <input class="form-control" type="text" placeholder="Address"/>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <div class="form-group">
+                        <label>Gender</label>
+                        <input class="form-control" type="text" placeholder="CHANGE"/>
+                    </div>
+                </div>            
+            </div>
+
+            <div class="row"> 
+                <div class="col-md-6 col-sm-12">
+                    <div class="form-group">
+                        <label>Driver's Licence Number</label>
+                        <input class="form-control" type="text" placeholder="Driver's Licence Number"/>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <div class="form-group">
+                        <label>Date of Birth</label>
+                        <input class="form-control" type="text" placeholder="CHANGE"/>
+                    </div>
+                </div>            
+            </div>
+
+            <div class="row"> 
+                <div class="col-md-6 col-sm-12">
+                    <div class="form-group">
+                        <label>Landline Number</label>
+                        <input class="form-control" type="text" placeholder="Landline Number"/>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <div class="form-group">
+                        <label>Mobile Number</label>
+                        <input class="form-control" type="text" placeholder="Mobile Number"/>
+                    </div>
+                </div>            
+            </div>
+
+            <div class="row"> 
+                <div class="col-md-6 col-sm-12">
+                    <div class="form-group">
+                        <label>Next of keen</label>
+                        <input class="form-control" type="text" placeholder="Next of keen"/>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <div class="form-group">
+                        <label>Relationship</label>
+                        <input class="form-control" type="text" placeholder="Relationship"/>
+                    </div>
+                </div>            
+            </div>
+
+            <div class="row"> 
+                <div class="col-md-6 col-sm-12">
+                    <div class="form-group">
+                        <label>Address</label>
+                        <input class="form-control" type="text" placeholder="Address"/>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <div class="form-group">
+                        <label>Mobile Number</label>
+                        <input class="form-control" type="text" placeholder="Mobile Number"/>
+                    </div>
+                </div>            
+            </div>
+
+            <br/>
+            <p><b>Position Details</b></p>
+
+            <div class="row"> 
+                <div class="col-md-6 col-sm-12">
+                    <div class="form-group">
+                        <label>Position Title</label>
+                        <input class="form-control" type="text" placeholder="Position Title"/>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <div class="form-group">
+                        <label>Department</label>
+                        <input class="form-control" type="text" placeholder="CHANGE"/>
+                    </div>
+                </div>            
+            </div>
+
+            <div class="row"> 
+                <div class="col-md-6 col-sm-12">
+                    <div class="form-group">
+                        <label>Employement Type</label>
+                        <input class="form-control" type="text" placeholder="CHANGE"/>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <div class="form-group">
+                        <label>Total Hours for each Week</label>
+                        <input class="form-control" type="text" placeholder="CHANGE"/>
+                    </div>
+                </div>            
+            </div>
+            <div class="row"> 
+                <div class="col-md-6 col-sm-12">
+                    <div class="form-group">
+                        <label>Gross Pay</label>
+                        <input class="form-control" type="text" placeholder="Gross Pay"/>
+                    </div>
+                </div>           
+            </div>
+
+            <br/>
+            <p><b>Education Details</b></p>
+            <div class="row"> 
+                <div class="col-md-8 col-sm-12">
+                    <div class="form-group">
+                        <label>Qualification</label>
+                        <input class="form-control" type="text" placeholder="Qualification"/>
+                    </div>
+                </div>
+                <div class="col-md-2 col-sm-12">
+                    <div class="form-group">
+                        <label>Year Completed</label>
+                        <input class="form-control" type="text" placeholder="CHANGE"/>
+                    </div>
+                </div>
+                <div class="col-md-2 col-sm-12">
+                    <div class="form-group">
+                        <label>GPA</label>
+                        <input class="form-control" type="text" placeholder="GPA"/>
+                    </div>
+                </div>              
+            </div>
+            <button type="button" class="btn btn-primary"><i class="fa fa-plus"></i> Add</button>
+
+            <br/>
+            <br/>
+            <p><b>Banking and Superannuation Details</b></p>
+            <div class="row"> 
+                <div class="col-md-6 col-sm-12">
+                    <div class="form-group">
+                        <label>Bank</label>
+                        <input class="form-control" type="text" placeholder="CHANGE"/>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <div class="form-group">
+                        <label>Bank Account Name</label>
+                        <input class="form-control" type="text" placeholder="Bank Account Name"/>
+                    </div>
+                </div>            
+            </div>
+
+            <div class="row"> 
+                <div class="col-md-6 col-sm-12">
+                    <div class="form-group">
+                        <label>Bank Account Number</label>
+                        <input class="form-control" type="text" placeholder="Bank Account Number"/>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <div class="form-group">
+                        <label>FNPF Number</label>
+                        <input class="form-control" type="text" placeholder="FNPF Number"/>
+                    </div>
+                </div>            
+            </div>
+
+
+
+            <button type="button" class="btn btn-success">Register</button> 
+            </form>
+        </div>
+    </div>
 </div>
-                        <div class="form-group">
-                            <label>Email</label>
-                            <input
-                                class="form-control"
-                                value="bootstrap@example.com"
-                                type="email"
-                            />
-                        </div>
-                        <div class="form-group">
-                            <label>URL</label>
-                            <input
-                                class="form-control"
-                                value="https://getbootstrap.com"
-                                type="url"
-                            />
-                        </div>
-                        <div class="form-group">
-                            <label>Telephone</label>
-                            <input
-                                class="form-control"
-                                value="1-(111)-111-1111"
-                                type="tel"
-                            />
-                        </div>
-                        <div class="form-group">
-                            <label>Password</label>
-                            <input class="form-control" value="password" type="password" />
-                        </div>
-                        <div class="form-group">
-                            <label>Readonly input</label>
-                            <input
-                                class="form-control"
-                                type="text"
-                                placeholder="Readonly input here…"
-                                readonly
-                            />
-                        </div>
-                        <div class="form-group">
-                            <label>Disabled input</label>
-                            <input
-                                type="text"
-                                class="form-control"
-                                placeholder="Disabled input"
-                                disabled=""
-                            />
-                        </div>
-                        <div class="form-group">
-                            <div class="row">
-                                <div class="col-md-6 col-sm-12">
-                                    <label class="weight-600">Custom Checkbox</label>
-                                    <div class="custom-control custom-checkbox mb-5">
-                                        <input
-                                            type="checkbox"
-                                            class="custom-control-input"
-                                            id="customCheck1"
-                                        />
-                                        <label class="custom-control-label" for="customCheck1"
-                                            >Check this custom checkbox</label
-                                        >
-                                    </div>
-                                    <div class="custom-control custom-checkbox mb-5">
-                                        <input
-                                            type="checkbox"
-                                            class="custom-control-input"
-                                            id="customCheck2"
-                                        />
-                                        <label class="custom-control-label" for="customCheck2"
-                                            >Check this custom checkbox</label
-                                        >
-                                    </div>
-                                    <div class="custom-control custom-checkbox mb-5">
-                                        <input
-                                            type="checkbox"
-                                            class="custom-control-input"
-                                            id="customCheck3"
-                                        />
-                                        <label class="custom-control-label" for="customCheck3"
-                                            >Check this custom checkbox</label
-                                        >
-                                    </div>
-                                    <div class="custom-control custom-checkbox mb-5">
-                                        <input
-                                            type="checkbox"
-                                            class="custom-control-input"
-                                            id="customCheck4"
-                                        />
-                                        <label class="custom-control-label" for="customCheck4"
-                                            >Check this custom checkbox</label
-                                        >
-                                    </div>
-                                </div>
-                                <div class="col-md-6 col-sm-12">
-                                    <label class="weight-600">Custom Radio</label>
-                                    <div class="custom-control custom-radio mb-5">
-                                        <input
-                                            type="radio"
-                                            id="customRadio1"
-                                            name="customRadio"
-                                            class="custom-control-input"
-                                        />
-                                        <label class="custom-control-label" for="customRadio1"
-                                            >Toggle this custom radio</label
-                                        >
-                                    </div>
-                                    <div class="custom-control custom-radio mb-5">
-                                        <input
-                                            type="radio"
-                                            id="customRadio2"
-                                            name="customRadio"
-                                            class="custom-control-input"
-                                        />
-                                        <label class="custom-control-label" for="customRadio2"
-                                            >Or toggle this other custom radio</label
-                                        >
-                                    </div>
-                                    <div class="custom-control custom-radio mb-5">
-                                        <input
-                                            type="radio"
-                                            id="customRadio3"
-                                            name="customRadio"
-                                            class="custom-control-input"
-                                        />
-                                        <label class="custom-control-label" for="customRadio3"
-                                            >Or toggle this other custom radio</label
-                                        >
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label>Disabled select menu</label>
-                            <select class="form-control" disabled="">
-                                <option>Disabled select</option>
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label>input plaintext</label>
-                            <input
-                                type="text"
-                                readonly
-                                class="form-control-plaintext"
-                                value="email@example.com"
-                            />
-                        </div>
-                        <div class="form-group">
-                            <label>Textarea</label>
-                            <textarea class="form-control"></textarea>
-                        </div>
-                        <div class="form-group">
-                            <label>Help text</label>
-                            <input type="text" class="form-control" />
-                            <small class="form-text text-muted">
-                                Your password must be 8-20 characters long, contain letters
-                                and numbers, and must not contain spaces, special characters,
-                                or emoji.
-                            </small>
-                        </div>
-                        <div class="form-group">
-                            <label>Example file input</label>
-                            <input
-                                type="file"
-                                class="form-control-file form-control height-auto"
-                            />
-                        </div>
-                        <div class="form-group">
-                            <label>Custom file input</label>
-                            <div class="custom-file">
-                                <input type="file" class="custom-file-input" />
-                                <label class="custom-file-label">Choose file</label>
-                            </div>
-                        </div>
-                    </form>
-                    </div>
-				</div>
             
 							
 @endsection
